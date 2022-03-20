804e1d5117ae25fa3a7610d5e78c796f3cecea8c
IT21161674 <99235649+IT21161674@users.noreply.github.com>
2022-03-20 08:55:52 +0000

    answers for tutorial 6

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,17 @@
 #include "Student.h"
 #include <iostream>
+#include <cstring>
+
+using namespace std;
 
 // Assign studentId and name
-Student::assignDetails() {
-  
+void Student::assignDetails(int id, char *Name) {
+  studentId = id;
+  strcpy(name, Name);
 }
 
 // Display StudentId and Name
-Student::display() {
-  
+void Student::display() {
+  cout << "Student id : " << studentId << endl;
+  cout << "Student name : " << name << endl;
 }
